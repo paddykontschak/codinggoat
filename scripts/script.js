e192b53995329977a5269f8a038d89339c8533a1
Patrick Kontschak <patrick.kontschak@gmail.com>
2012-09-02 22:55:24 -0400

    Add CoffeeScript syntax highlighting
    The education page is now correctly colored, ajax-loading of the prism.js file was removed from the script.js and I added some empty lines to the skills for better readability.
    
    Signed-off-by: Patrick Kontschak <patrick.kontschak@gmail.com>

--- scripts/script.js
+++ scripts/script.js
@@ -11,13 +11,6 @@
     });
     $("#skills").on("click", function(event) {
       $("#scroller").load("skills.html");
-      $.getScript("scripts/prism.js", function(data, textStatus, jqxhr) {
-        console.log(data);
-        console.log(textStatus);
-        console.log(jqxhr.status);
-        console.log("Load was performed.");
-        return 0;
-      });
       return 0;
     });
     $("#education").on("click", function(event) {
